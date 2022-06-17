6a4ec87f473860c38ff3133daff266751df7972b
Larry <newman@greatbigcloud.com>
2022-06-16 23:07:11 -0500

    Catching up

--- Net Ninja/ModernJavaScript/Lesson_58/sandbox.js
+++
@@ -1,7 +0,0 @@
-const link = document.querySelector('a');
-console.log(link.getAttribute('href'));
-link.setAttribute('href', 'https://www.thenetninja.co.uk');
-link.innerText = 'The Net Ninja Website';
-
-const mssg = document.querySelector('p');
-console.log(mssg.getAttribute);
